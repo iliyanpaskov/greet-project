042a69c33354a2c82bcb3b12eda11768050e40d7
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-10-10 01:32:52 +0300

    Components folder and global-styles folder created

---
+++ src/components/common/CAddToCartButton/CAddToCartButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CAddToCartButton.scss';
+
+interface CAddToCartButtonProps { }
+
+export const CAddToCartButton: React.FC<CAddToCartButtonProps> = () => {
+    return (
+        <div>CAddToCartButton</div>
+    )
+}
